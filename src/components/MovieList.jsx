ed91ba1d9b532a7bf9be8af7ebe81425ac587fa1
Uri Mikhli <urimikhli@gmail.com>
2021-04-14 20:55:12 -0400

    Showing a list of canned data and click on link to get the detail from omdbapi TODO: - get search data from omdbap - BUG:List is constantly fetching the data, should only do it once.

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,7 +1,53 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import {
+  useParams,
+  Link
+} from "react-router-dom";
 
-const MovieList = (props) => {
-  return <div>List of movies goes here</div>
+const MovieList = () => {
+  let { search } = useParams();
+  let searchParams = ''
+  if (search) searchParams = "?search=" + search
+  const protocol = 'http://'
+  const apiurl = '127.0.0.1:5000/movie/list'
+
+  const [movieList, setMovieData] = useState({})
+
+  useEffect(() => {
+    fetch(`${protocol}${apiurl}${searchParams}`)
+      .then(res => res.json())
+      .then(data => {
+        setMovieData(data);
+      })
+      .catch(err => console.error(err));
+  });
+
+
+  if (Object.keys(movieList).length === 0) {
+    return <div>
+
+      <h1>Search on Title: coming soon</h1>
+    </div>;
+  } else {
+
+    const localList = JSON.parse(JSON.stringify(movieList));
+
+    return <div>
+      <h1>Search results</h1>
+      {
+        localList.map((movieData, index) => (
+          <div key={index}>
+            <div> <Link to={{
+              pathname: "/movie/" + movieData.id
+            }}
+            >{movieData.movieTitle}</Link> {movieData.movieYear}</div>
+          </div>
+        ))
+      }
+
+
+  </div>
+  }
 }
 
 export default MovieList;
--- transformApp/api.py
+++ transformApp/api.py
@@ -4,6 +4,7 @@ import json
 import flask
 from flask import jsonify
 from flask_cors import CORS, cross_origin
+from flask import request
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
@@ -23,11 +24,38 @@ sampleMovieDetail = {
   "contentType": "movie"
 }
 
+sampleMovieList = [
+    {
+        "id": "tt0110357",
+        "movieTitle": "The Lion King",
+        "movieYear": "1994",
+        "movieLength": "88 min",
+        "moviePlot": "Lion prince Simba and his father are targeted by his bitter uncle, who wants to ascend the throne himself.",
+        "moviePoster": "https://m.media-amazon.com/images/M/MV5BYTYxNGMyZTYtMjE3MS00MzNjLWFjNmYtMDk3N2FmM2JiM2M1XkEyXkFqcGdeQXVyNjY5NDU4NzI@._V1_SX300.jpg",
+        "contentType": "movie"
+    },
+    {
+        "id": "tt3896198",
+        "movieTitle": "Guardians of the Galaxy Vol. 2",
+        "movieYear": "2017",
+        "movieLength": "136 min",
+        "moviePlot": "The Guardians struggle to keep together as a team while dealing with their personal family issues, notably Star-Lord's encounter with his father the ambitious celestial being Ego.",
+        "moviePoster": "https://m.media-amazon.com/images/M/MV5BNjM0NTc0NzItM2FlYS00YzEwLWE0YmUtNTA2ZWIzODc2OTgxXkEyXkFqcGdeQXVyNTgwNzIyNzg@._V1_SX300.jpg",
+        "contentType": "movie",
+    }
+]
+
 
 @app.route('/movie/list', methods=['GET'])
 @cross_origin()
 def movie_list():
-    return "<p>Returns a list of movies</p>"
+    params = request.args #.get('username')
+    print(params)
+    for movie in sampleMovieList:
+      jsonify(movie)
+    
+    print(sampleMovieList)
+    return jsonify(sampleMovieList)
 
 @app.route('/movie/detail/<string:movie_id>', methods=['GET'])
 @cross_origin()
